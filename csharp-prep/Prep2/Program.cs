6c0342ffb6510dbefc343bce6f7cc43d724fcbc5
Ezekiel Wilcox <ezekielwilcox@icloud.com>
2025-01-11 21:32:24 -0700

    completed assignments 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please state your grade percentage:");
+        string percentAnswer = Console.ReadLine();
+        int percent = int.Parse(percentAnswer);
+
+        string grade = "";
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else grade = "F";
+
+        Console.WriteLine($"Your grade is {grade}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrtulations! You passed!");
+        }
+        else Console.WriteLine("Sorry, you falied. Do better.");
     }
 }
