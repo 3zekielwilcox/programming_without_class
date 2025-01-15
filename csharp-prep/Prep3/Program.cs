1800eee7cb33737bba686cac13d32e99dd588e10
Ezekiel Wilcox <ezekielwilcox@icloud.com>
2025-01-15 16:23:50 -0700

    wrote guess number game in c#.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the GUESS THE NUMBER game!");
+        Console.WriteLine("In this game, you try to guess a random number in the least attempts.");
+        Console.WriteLine();
+        Console.WriteLine("Now, pleaase choose the maximum amount of positive numbers to choose from:");
+
+        int response = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, response);
+
+        Console.WriteLine();
+        Console.WriteLine("A random number has been chosen.");
+
+        int guess = -1;
+        int count = 0;
+        while (guess != randomNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > randomNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Lower.");
+                count ++;
+            }
+
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Higher.");
+                count ++;
+            }
+
+            else if (guess == randomNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Congratulations! You guessed the number!");
+                count ++;
+            }
+
+            else
+            {
+                Console.WriteLine("Something went wrong. Please guess again.");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"The nuber was {randomNumber}.");
+        Console.WriteLine($"It took you {count} guess(es).");
     }
 }
