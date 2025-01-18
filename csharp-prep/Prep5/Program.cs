3bbd0c76a2d7b5757fd514c9237fd03fde6436a6
Ezekiel Wilcox <ezekielwilcox@icloud.com>
2025-01-17 23:01:13 -0700

    completed c# prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DislayWelcome();
+        string name = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squared = SquareNumber(userNumber);
+        DisplayResult(name, squared);
+    }
+    static void DislayWelcome()
+    {
+        Console.WriteLine("welcome to the program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int squared = userNumber * userNumber;
+
+        return squared;
+    }
+
+    static void DisplayResult(string name, int squared)
+    {
+        Console. WriteLine($"{name}, the square of your favorite number is {squared}.");
     }
 }
